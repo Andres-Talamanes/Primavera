582626cd6fd24a895ae6ef04436649732730cc80
LuisMurSal <95453620+LuisMurSal@users.noreply.github.com>
2021-12-08 18:50:11 -0800

    Add files via upload

---
+++ proyecto/Arbol.java
@@ -0,0 +1,39 @@
+public class Arbol {
+
+    private Trabajador raiz;
+
+
+    public Arbol(Trabajador ID)
+    {
+        this.raiz = new Trabajador(null, null, null, 0, null, 0, 0, null);
+    }
+
+    public Arbol() {
+    }
+
+    public Trabajador getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Trabajador raiz) {
+        this.raiz = raiz;
+    }
+
+    public boolean esVacio()
+    {
+        return raiz==null;
+    }
+
+    //Recorrer arbol InOrden
+    public void inOrden(Trabajador r)
+    {
+        if(r!=null)
+        {
+            inOrden(r.getIzquierdo());
+            System.out.print(r.getID() + "\t");
+            inOrden(r.getDerecho());
+        }
+    }
+
+    
+}
